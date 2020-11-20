292ac0131384f95a8b3052cdae6f74f693ee3d78
unknown <[vikaromansova@gmail.com]>
2020-11-20 16:26:07 +0200

    hw 5 task

--- hw1/index.js
+++ hw1/index.js
@@ -1,41 +1,41 @@
-//hw1.1
-let number = 10,
-string = 'Vika',
-//null = null,
-bool = true,
-und ;
-let user = {
-    name: 'Vika',
-    age: 18
-}
-let arr = [1,2,3,4,5,6,7,8];
+// //hw1.1
+// let number = 10,
+// string = 'Vika',
+// //null = null,
+// bool = true,
+// und ;
+// let user = {
+//     name: 'Vika',
+//     age: 18
+// }
+// let arr = [1,2,3,4,5,6,7,8];
 
-//hw1.2
+// //hw1.2
 
-let user_Vasya = {
-    name:'Vasya',
-    age:25,
-    man:true,
-    childrens_name: {
-        ch1:'petya',
-        ch2:'oleg'
-    },
-    childrens_age: [12,19],
-    getall: function () {
-        const template = `
-        name: ${user_Vasya.name}
-        age: ${user_Vasya.age}
-        man: ${user_Vasya.man}
-        children_name: ${user_Vasya.childrens_name.ch1}
-        children_age: ${user_Vasya.childrens_age}
+// let user_Vasya = {
+//     name:'Vasya',
+//     age:25,
+//     man:true,
+//     childrens_name: {
+//         ch1:'petya',
+//         ch2:'oleg'
+//     },
+//     childrens_age: [12,19],
+//     getall: function () {
+//         const template = `
+//         name: ${user_Vasya.name}
+//         age: ${user_Vasya.age}
+//         man: ${user_Vasya.man}
+//         children_name: ${user_Vasya.childrens_name.ch1}
+//         children_age: ${user_Vasya.childrens_age}
 
-        `
-        console.log(template)
-        return;
-    }
+//         `
+//         console.log(template)
+//         return;
+//     }
     
-}
-user_Vasya.getall()
+// }
+// user_Vasya.getall()
 
 //hw1.3
 //  let getuser = function() {
@@ -45,3 +45,45 @@ user_Vasya.getall()
 // }
 // getuser();
 
+//hw1.5
+const usersArr = [
+    {
+        name:'Vika',
+        age:18,
+        sername:'Romantsova',
+        birhsday:'30.11.2001'
+    },
+    {
+        name:'Oleg',
+        age:38,
+        sername:'firsov',
+        birhsday:'30.11.1987'
+    },
+    {
+        name:'Vasya',
+        age:30,
+        sername:'Olegovich',
+        birhsday:'30.1.1990'
+    },
+    {
+        name:'Petya',
+        age:22,
+        sername:'Romanov',
+        birhsday:'30.11.1998'
+    }
+]
+// const getUser = function (index){
+    
+//     console.log(usersArr[index]);
+// }
+const getUser = function(name){
+    for(let i = 0; i < usersArr.length; i++){
+        const user = usersArr[i];
+        if(name == user.name){
+            console.log(user);
+            break;
+        }
+        continue;
+    }
+   //console.log(user)
+}
