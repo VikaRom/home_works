bc0110ba7b599c21089573b86efe1614b770d595
unknown <[vikaromansova@gmail.com]>
2020-11-20 13:32:43 +0200

    hw first three tasks

---
+++ hw1/index.js
@@ -0,0 +1,32 @@
+//hw1.1
+let number = 10,
+string = 'Vika',
+//null = null,
+bool = true,
+und ;
+let user = {
+    name: 'Vika',
+    age: 18
+}
+let arr = [1,2,3,4,5,6,7,8];
+
+//hw1.2
+
+let user_Vasya = {
+    name:'Vasya',
+    age:25,
+    man:true,
+    childrens_name: {
+        ch1:'petya',
+        ch2:'oleg'
+    },
+    childrens_age: [12,19]
+}
+
+//hw1.3
+ let getuser = function() {
+   console.log(user_Vasya);
+   return;
+
+}
+getuser();
